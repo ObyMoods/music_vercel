e68c127d3abb8c4cffcd1207e63c8511ee4caf8c
ObyMoods <aobi5198@gmail.com>
2026-04-25 13:15:53 +0700

    Upload api/search.js

---
+++ api/search.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+
+module.exports = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+    
+    const { q, token } = req.query;
+    
+    if (!token) {
+        return res.status(401).json({ error: 'No token provided' });
+    }
+    
+    try {
+        const response = await axios.get('https://api.spotify.com/v1/search', {
+            params: {
+                q: q,
+                type: 'track,artist,playlist',
+                limit: 20
+            },
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        
+        res.json(response.data);
+    } catch (error) {
+        res.status(error.response?.status || 500).json({ error: error.message });
+    }
+};
